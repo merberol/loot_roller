c28f0440ad372edefa76eaf24327f20af95234d6
Charlie Simonsson <charlie.simonsson@liu.se>
2024-10-24 10:13:21 +0200

    removed __pucache__ and added .gitignore

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 from data import treashuretable_3_5 as t3_5
 import json
+import random as rand
+
+
+def is_in(a_string : str, roll : int) -> bool:
+    val = eval(a_string)
+    print(val, roll)
+    assert isinstance(val, tuple), "val is not tuple something went wrong"
+    if not val:
+        return False
+    res = val[0] <= roll and roll <= val[1]
+    print(res)
+    return res
+
+
+def roll_random(level):
+    res = {}
+    table = t3_5.TABLE[level]
+    for key, value in table.items():
+        print("rolling for " + key)
+        roll = rand.randint(1,100)
+        print(roll)
+        for cand , val in value.items():
+            if is_in(cand, roll):
+                res[key] = val
+                break
+        else:
+            print("Something went wrong", res)
+            exit()
+                
 
 
 def main():
-    new = {}
-    for key,value in t3_5.TABLE.items():
-        new_val = {}
 
-        for key2, value2 in value.items():
-            key2 = str(key2)
-            new_value2 = {}
-            for key3, value3 in value2.items():
-                key3 = str(key3)
-                new_value2[key3] = value3
-            new_val[str(key2)] = new_value2
-        
-        new[str(key)] = new_val
-                
-
-    f = open("data/t3_5.json", "w")
-    json.dump(new, f)
-    f.close
+    print(roll_random(1))
+    
 
 if __name__ == "__main__":
     main()
